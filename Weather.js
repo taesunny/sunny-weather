6e491512181a08466ff433e4b406d0ccf4acedaf
Taesun Lee <superbsun@gmail.com>
2020-04-17 00:52:57 +0900

    add icon and styling

--- Weather.js
+++ Weather.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function Weather({ temp }) {
   return (
     <View style={styles.container}>
-      <Text>{temp}</Text>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons
+          size={96}
+          name="weather-lightning-rainy"
+        ></MaterialCommunityIcons>
+        <Text style={styles.temp}>{temp}˚</Text>
+      </View>
+      <View style={styles.halfContainer}>
+        <Text>{temp}</Text>
+      </View>
     </View>
   );
 }
@@ -23,11 +33,19 @@ Weather.propTypes = {
     "Haze",
     "Mist",
     "Dust",
-  ]).isRequired
+  ]).isRequired,
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 42,
+  },
+  halfContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
